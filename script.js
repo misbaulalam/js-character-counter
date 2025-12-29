5ecb6a634fc9a42e859132c9375f7f885eebb902
misbaulalam <123174596+misbaulalam@users.noreply.github.com>
2025-12-29 18:28:39 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let inp = document.querySelector("input");
+let span = document.querySelector("span");
+
+let textLength = 15;
+span.textContent = textLength;
+
+inp.addEventListener("input", function(evt){
+    let charleft = textLength-inp.value.length;
+    span.textContent = charleft;
+    if(charleft<0){
+        span.style.color = "red";
+    }else{
+        span.style.color= "whitesmoke"
+    }
+
+})
